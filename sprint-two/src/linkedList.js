12518b48db67a2fbb36b92ba2cedf85f873885e9
pafrias <pafrias90@gmail.com>
2019-01-05 15:55:05 -0800

    2/3 through LinkedList Implementation

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,76 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    
+    var x = Node(value);
+    
+    if (!this.tail) {
+      this.head = x;
+      this.tail = x;
+    } else {
+      this.tail.next = x;
+      this.tail = x;
+    }
+    
+    /*
+    
+    #save new Node(value) as a variable
+    #catch if no current tail/1st time
+    #  update head and tail
+    #  else update tail
+    #list.tail points to new tail
+    #list.tail becomes the new tail
+    
+    attach new Node to list
+    
+    */
+    
   };
 
   list.removeHead = function() {
+    var oldHead = this.head.value;
+    this.head = this.head.next;
+    return oldHead;
+    
+    /*
+    
+    save current head as oldHead
+    this.head becomes this.head.next
+    
+    return oldHead
+    */
+    
   };
 
   list.contains = function(target) {
+    if(arguments[1]) {
+      if(arguments[1].value === target) {
+        return true;
+      } else {
+        
+      }
+    }
+    
+    /*
+    if (arg[1]) {
+      check arg1.value
+      retrn true
+      if no match and arg1.next
+        recurse on arg1.next
+      
+    } else
+    
+    if head.value is target
+      return true
+      check head for target
+    */
+    
+    /* 
+    if this.value exists
+      check if it matches
+    else 
+      recurse
+    */
   };
 
   return list;
