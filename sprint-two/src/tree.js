e7d6f3c18bf19b83e92e012065b30b332962b0d0
pafrias <pafrias90@gmail.com>
2019-01-05 18:09:43 -0800

    Finished Trees

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,20 +1,35 @@
+
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length > 0) {
+    return _.reduce(this.children, function(memo, child) {
+      if (memo || child.value === target) {
+        return true;
+      } else {
+        return child.contains(target);
+      }
+    }, false);
+  } else {
+    return false;
+  }
 };
+  
+
 
 
 
